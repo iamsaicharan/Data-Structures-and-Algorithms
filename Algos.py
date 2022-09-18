6a4da413ba6260e5dd7e3e111fa4cd463b188303
iamsaicharan <charanvadakapur@gmail.com>
2022-09-18 16:11:33 -0400

    added the linear and binary operations

--- Algos.py
+++ Algos.py
@@ -5,7 +5,22 @@ def LinearSearch(numbersList, key):
             return i
     return -1
 
+def BinarySearch(numbersList, key):
+    left = 0
+    right = len(numbersList) - 1
+    while right>=left:
+        mid = (left + right)//2
+        if numbersList[mid] < key:
+            left = mid + 1
+        elif numbersList[mid] > key:
+            right = mid - 1
+        else:
+            return mid
+    return -1
+        
+
 
 
 numbers = [2, 4, 7, 10, 11, 32, 45, 87]
-print(LinearSearch(numbers, 10))
+print(LinearSearch(numbers, 45))
+print(BinarySearch(numbers, 45))
